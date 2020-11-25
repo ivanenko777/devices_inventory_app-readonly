3a6a942bbfd8a0d30cb13bb706088e36647ff614
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-11-25 21:22:27 +0200

    create UsersController action index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'home/index'
-  resources :users
+  resources :users#, except: [:show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
