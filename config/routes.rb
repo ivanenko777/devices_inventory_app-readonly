2e37d9db8b8b152c1e0c89f1d9fd8609416da5ee
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-11-26 00:04:41 +0200

    create login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
+
   get 'home/index'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
