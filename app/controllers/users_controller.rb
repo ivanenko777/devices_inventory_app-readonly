fcf16d531d75f35affa9e7ea5ee442d9b3f12784
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-11-25 22:18:53 +0200

    create UsersController action new create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,28 @@ class UsersController < ApplicationController
   # GET /users/1
   def show; end
 
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new
+    end
+  end
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:full_name, :email, :is_active, :password, :password_confirmation)
   end
 end
