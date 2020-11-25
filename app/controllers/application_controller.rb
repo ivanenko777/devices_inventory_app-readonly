772ab4f3470d5f1ad4c0f65f4b159f88c70b18f7
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-11-26 00:11:31 +0200

    display login and logout links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user
+  helper_method :logged_in?
+
+  def current_user
+    session[:user_id] ? User.find_by(id: session[:user_id]) : nil
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
