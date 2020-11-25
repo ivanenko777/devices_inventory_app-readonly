e17f5970981c353231ad34af52cd43dff3ca7d3b
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-11-25 21:30:26 +0200

    create UsersController action show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  # GET /users
   def index
     @users = User.all
+  end
+
+  # GET /users/1
+  def show; end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
