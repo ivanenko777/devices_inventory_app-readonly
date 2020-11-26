feb94c9ceb7a1a50de43c34051df11f0d34f1851
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-11-26 11:50:37 +0200

    create Office model
    rails g model office name:string address:string is_active:boolean
    
    (cherry picked from commit da4bbafaf198aae2677d4516c3b47df8d4ab68fb)

---
+++ app/models/office.rb
@@ -0,0 +1,4 @@
+class Office < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_092427) do
+ActiveRecord::Schema.define(version: 2020_11_26_094422) do
+
+  create_table "offices", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "full_name"
