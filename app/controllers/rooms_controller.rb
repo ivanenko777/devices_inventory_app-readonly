e5cd8bc205186a52ab37927eac29a89f3ccd5a5b
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-11-26 21:13:19 +0200

    create RoomsController action show
    (cherry picked from commit 74325a37c7581a2f7d220fe61a5ed6c083df3a6a)

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,17 @@
 class RoomsController < ApplicationController
+  before_action :set_room, only: [:show]
+
+  # GET /rooms
   def index
     @rooms = Room.all
+  end
+
+  # GET /rooms/1
+  def show; end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
   end
 end
