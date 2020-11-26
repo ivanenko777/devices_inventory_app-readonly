4fad5239a0aacca273d93c4a3d4fd1ec23efda6c
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-11-26 12:25:42 +0200

    create OfficesController action show
    (cherry picked from commit dca71167c90fdb8202eee5747dae98afda6f7de0)

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,7 +1,17 @@
 class OfficesController < ApplicationController
+  before_action :set_office, only: [:show]
 
   # GET /offices
   def index
     @offices = Office.all
+  end
+
+  # GET /offices/1
+  def show; end
+
+  private
+
+  def set_office
+    @office = Office.find(params[:id])
   end
 end
