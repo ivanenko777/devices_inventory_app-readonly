45e9a95e1c4c0e87d623ad8df94ea483a2d5c889
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-11-30 22:18:43 +0200

    create DeviceTypesController actions new create

--- app/controllers/device_types_controller.rb
+++ app/controllers/device_types_controller.rb
@@ -9,9 +9,29 @@ class DeviceTypesController < ApplicationController
   # GET /device_types/1
   def show; end
 
+  # GET /device_types/new
+  def new
+    @device_type = DeviceType.new
+  end
+
+  # POST /device_type
+
+  def create
+    @device_type = DeviceType.new(device_type_params)
+    if @device_type.save
+      redirect_to @device_type, notice: 'Device manufacturer was successfully created.'
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_device_model
     @device_type = DeviceType.find(params[:id])
+  end
+
+  def device_type_params
+    params.require(:device_type).permit(:sys_name, :name)
   end
 end
