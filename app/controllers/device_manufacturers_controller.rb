c507a570031e547e62fca739f452d9f685c49f8e
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-11-30 16:05:19 +0200

    create DeviceManufacturersController actions new create

--- app/controllers/device_manufacturers_controller.rb
+++ app/controllers/device_manufacturers_controller.rb
@@ -8,9 +8,26 @@ class DeviceManufacturersController < ApplicationController
 
   def show; end
 
+  def new
+    @manufacturer = DeviceManufacturer.new
+  end
+
+  def create
+    @manufacturer = DeviceManufacturer.new(device_manufacturer_params)
+    if @manufacturer.save
+      redirect_to @manufacturer, notice: 'Device manufacturer was successfully created.'
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_manufacturer
     @manufacturer = DeviceManufacturer.find(params[:id])
+  end
+
+  def device_manufacturer_params
+    params.require(:device_manufacturer).permit(:name)
   end
 end
