c1963b235ac919047d81671c7172a93472f0c0ff
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-11-30 22:09:10 +0200

    create DeviceTypesController action show

--- app/controllers/device_types_controller.rb
+++ app/controllers/device_types_controller.rb
@@ -1,7 +1,17 @@
 class DeviceTypesController < ApplicationController
+  before_action :set_device_model, only: [:show]
 
   # GET /device_types
   def index
     @device_types = DeviceType.all
+  end
+
+  # GET /device_types/1
+  def show; end
+
+  private
+
+  def set_device_model
+    @device_type = DeviceType.find(params[:id])
   end
 end
