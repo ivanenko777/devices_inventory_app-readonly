f792dff83144db57c8462dd2dc5af8f6b98da681
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-11-30 15:38:54 +0200

    create DeviceManufacturersController action show

--- app/controllers/device_manufacturers_controller.rb
+++ app/controllers/device_manufacturers_controller.rb
@@ -1,6 +1,16 @@
 class DeviceManufacturersController < ApplicationController
 
+  before_action :set_manufacturer, only: [:show]
+
   def index
     @manufacturers = DeviceManufacturer.all
+  end
+
+  def show; end
+
+  private
+
+  def set_manufacturer
+    @manufacturer = DeviceManufacturer.find(params[:id])
   end
 end
