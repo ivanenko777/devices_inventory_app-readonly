0e2ee8526680dd0052eb40fe93752d5458b1b32f
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-12-01 13:31:25 +0200

    fix validation

--- app/models/device_model.rb
+++ app/models/device_model.rb
@@ -3,6 +3,6 @@ class DeviceModel < ApplicationRecord
   belongs_to :device_type
 
   validates :name, presence: true, uniqueness: {scope: [:device_manufacturer_id, :device_type_id]}
-  validates :device_manufacturer_id
-  validates :device_type_id
+  validates :device_manufacturer_id, presence: true
+  validates :device_type_id, presence: true
 end
