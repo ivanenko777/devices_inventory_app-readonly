5a14f531f23cd424e1b40e00153c9a01cd0fa750
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-12-01 14:06:01 +0200

    create DeviceModelsController action show

--- app/controllers/device_models_controller.rb
+++ app/controllers/device_models_controller.rb
@@ -1,7 +1,17 @@
 class DeviceModelsController < ApplicationController
+  before_action :set_device_model, only: [:show]
 
   # GET /device_models/index
   def index
     @device_models = DeviceModel.includes(:device_type, :device_manufacturer).all
+  end
+
+  # GET /device_models/1
+  def show; end
+
+  private
+
+  def set_device_model
+    @device_model = DeviceModel.find(params[:id])
   end
 end
