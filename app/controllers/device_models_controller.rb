5cc1f308867ed7f2eab8fb3e4ac098ec01e137ad
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-12-01 14:02:15 +0200

    Eager Loading

--- app/controllers/device_models_controller.rb
+++ app/controllers/device_models_controller.rb
@@ -2,6 +2,6 @@ class DeviceModelsController < ApplicationController
 
   # GET /device_models/index
   def index
-    @device_models = DeviceModel.all
+    @device_models = DeviceModel.includes(:device_type, :device_manufacturer).all
   end
 end
