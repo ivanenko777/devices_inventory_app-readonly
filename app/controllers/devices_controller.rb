31408a03f6b082531c63f7d3191f0b90c82a2326
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-12-02 14:00:37 +0200

    create DevicesController action show

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,7 +1,17 @@
 class DevicesController < ApplicationController
+  before_action :set_device, only: [:show]
 
   # GET /devices
   def index
     @devices = Device.includes(:device_model, room: :office).all
+  end
+
+  # GET /devices/1
+  def show; end
+
+  private
+
+  def set_device
+    @device = Device.find(params[:id])
   end
 end
