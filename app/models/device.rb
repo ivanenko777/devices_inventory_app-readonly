d1f0643cc1b16098f26fe273f949b3fe0806c6a8
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-12-05 22:43:20 +0200

    fix AR join

--- app/models/device.rb
+++ app/models/device.rb
@@ -11,8 +11,8 @@ class Device < ApplicationRecord
 
   scope :filter_by_status, ->(status_id) { where status: status_id }
   scope :filter_by_device_type, ->(device_type_id) { joins(device_model: :device_type).where("device_models.device_type_id = ?", device_type_id) }
-  scope :filter_by_device_manufacturer, lambda { |device_manufacturer_id|
-    joins(device_model: :device_manufacturer).where('device_models.device_manufacturer_id = ?', device_manufacturer_id)
+  scope :filter_by_device_manufacturer, ->(device_manufacturer_id) {
+    joins(:device_model).where('device_models.device_manufacturer_id = ?', device_manufacturer_id)
   }
   scope :filter_by_device_model, ->(device_model_id) { where(device_model_id: device_model_id) }
 
