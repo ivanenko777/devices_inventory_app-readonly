097cf7b29aa58c42b86ef4a33ed2eab3770efe9e
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-12-06 23:40:45 +0200

    devideHistory validation

--- app/models/device_history.rb
+++ app/models/device_history.rb
@@ -1,4 +1,9 @@
 class DeviceHistory < ApplicationRecord
   belongs_to :device
   belongs_to :room
+
+  validates :device_id, presence: true
+  validates :room_id, presence: true
+  validates :status_id, presence: true
+  validates :created_by_id, presence: true
 end
